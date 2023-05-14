c6ae33cabf51d26941575f14387af85c004f154a
vinit <vinitkkulkarni@gmail.com>
2023-05-14 09:03:10 +0530

    Added:new contact method call

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("*** ADDRESS BOOK ***");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
     }
 }
